efccf057a9f21a6352ee9aeb13fbe3fa46d2e5a6
pkamanthe <patriciamumbua97@gmail.com>
2024-10-29 22:08:43 +0300

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,16 @@
-import React from "react";
-import Article from './Article'
-import Header from './Header'
+import React from 'react';
+import Article from './Article';
+import Header from './Header';
 
 function App() {
-  return <div className="App">
-    learn react!
-    <Header/>
-    <Article />
-  </div>;
+  return (
+    <div className="App">
+      Learn React!
+      <Header>Hello!</Header>   
+
+      <Article />
+    </div>
+  );
 }
 
 export default App;
